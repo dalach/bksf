6b6e5d41440938b353ebf2a4c65e2d7bb545d298
dalach <wikadalach@gmail.com>
2014-06-19 20:41:29 +0200

    correct Book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,4 @@
 class Book < ActiveRecord::Base
   validates :title, :author, presence: true
-  validates_format_of :cover_photo, with: %r{\.gif|jpg|png}i, :message => "Upload a cover photo jpg/jpeg/png"  
+  validates_format_of :cover, with: %r{\.gif|jpg|png}i, :message => "Upload a cover photo jpg/jpeg/png"  
 end
