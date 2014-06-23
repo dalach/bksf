b7db1abd6fea6d1b146fd3b4ab70c7302e8dabb7
dalach <wikadalach@gmail.com>
2014-06-23 11:49:47 +0200

    add picture upload

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ActiveRecord::Base
+  mount_uploader :cover, CoverUploader
   validates :title, :author, presence: true
   validates_format_of :cover, with: %r{\.gif|jpg|png}i, :message => "Upload a cover photo jpg/jpeg/png"  
 end
