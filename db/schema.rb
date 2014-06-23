5b8046e72a6b7b52ce4d6b20a21be223e7f1f350
dalach <wikadalach@gmail.com>
2014-06-23 12:01:40 +0200

    migrate DB - bookshelves table

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140619174653) do
+ActiveRecord::Schema.define(version: 20140623095246) do
 
   create_table "books", force: true do |t|
     t.string   "title"
     t.string   "author"
     t.string   "cover"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "bookshelves", force: true do |t|
+    t.string   "name"
+    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
