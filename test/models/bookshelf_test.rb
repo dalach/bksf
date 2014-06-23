00e02e235ac5392780bdcc724ac8b42cf047f785
dalach <wikadalach@gmail.com>
2014-06-23 12:02:35 +0200

    test Bookshelf model

--- test/models/bookshelf_test.rb
+++ test/models/bookshelf_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class BookshelfTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should be valid" do
+    assert bookshelves(:one).valid?
+  end
+
+  test "shoukd have name" do
+    bookshelves(:one).name = nil
+    assert_not bookshelves(:one).valid?
+  end
 end
