92e92a55dfd9b4005e2e3d577eb732403421691a
dalach <wikadalach@gmail.com>
2014-06-26 00:04:14 +0200

    add premalinks

--- app/models/bookshelf.rb
+++ app/models/bookshelf.rb
@@ -3,4 +3,13 @@ class Bookshelf < ActiveRecord::Base
   has_many :book_on_bookshelves
   has_many :books, :through => :book_on_bookshelves
   belongs_to :user
+
+  def to_param
+    permalink
+  end
+
+  def permalink
+    "#{user.id}-#{name.parameterize}"
+  end
+
 end
