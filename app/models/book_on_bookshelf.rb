3b0d49003f734a5a03182e37daa7737f8d2fd395
dalach <wikadalach@gmail.com>
2014-07-01 22:25:09 +0200

    [err] show books from bookshelf

--- app/models/book_on_bookshelf.rb
+++ app/models/book_on_bookshelf.rb
@@ -1,4 +1,14 @@
 class BookOnBookshelf < ActiveRecord::Base
   belongs_to :book
   belongs_to :bookshelf
+
+  def user_bookshelf
+    BookOnBookshelf.all.select{ |bookOnBookshelf| bookOnBookshelf.bookshelf.id == current_user.bookshelf.id }
+  end
+
+  def user_books
+    user_bookshelf.each do |bookOnBookshelf|
+      Books.all.select {|book| book.id == bookOnBookshelf.book_id}
+    end
+  end
 end
