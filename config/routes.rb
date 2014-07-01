daa0eadf268c6839a2b85bd1770e88667210ccfa
dalach <wikadalach@gmail.com>
2014-07-01 18:41:54 +0200

    create about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   root :to => redirect('/bookshelves')
+  get '/about', to: "about#about", :as => :about
+
   devise_for :users
   resources :bookshelves
   resources :books
   resources :books do
     get :add_to_bookshelf, :to => :show
   end
-
 end
